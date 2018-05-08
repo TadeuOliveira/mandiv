6dbcfbc9d481cc9633dda3dfb7a13dc57ddf5b79
Tadeu <tadeu_oliveira@id.uff.br>
2018-05-08 18:25:51 -0300

    cadastro

--- contracts/Mandiv.sol
+++ contracts/Mandiv.sol
@@ -2,16 +2,28 @@ pragma solidity ^0.4.17;
 
 contract Mandiv{
 
+  //structs
   struct Usuario{
     string nome;
+    string username;
     bool motorista;
     uint avaliacao;
+    string endereco;
   }
 
+  //variaveis
   mapping(address=>Usuario) public usuarios;
 
-  function cadastrar(string _nome,bool _motorista) public {
+  //eventos
+
+
+  //funcoes
+  function cadastrar(string _nome, string _username, bool _motorista) public {
     require(usuarios[msg.sender].avaliacao == 0);
-    usuarios[msg.sender] = Usuario(_nome,_motorista,100);
+    usuarios[msg.sender] = Usuario(_nome,_username,_motorista,100);
   }
+
+  function compareStrings (string a, string b) view returns (bool){
+       return keccak256(a) == keccak256(b);
+   }
 }
